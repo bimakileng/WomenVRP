dfeacbd707bac49d2dc8267be5a5aa949fc85adf
nanjekyejoannah <nanjekyejoannah@yahoo.com>
2014-11-14 17:44:22 +0300

    added configuration for compiling assets

--- config/application.rb
+++ config/application.rb
@@ -2,6 +2,7 @@ require File.expand_path('../boot', __FILE__)
 
 require 'rails/all'
 
+
 # Require the gems listed in Gemfile, including any gems
 # you've limited to :test, :development, or :production.
 Bundler.require(*Rails.groups)
@@ -15,3 +16,5 @@ module DFW
 	config.assets.precompile << /\.(?:svg|eot|woff|ttf)$/
   end
 end
+
+config.serve_static_assets = true
