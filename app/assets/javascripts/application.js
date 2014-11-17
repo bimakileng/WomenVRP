916274175911e243490cbbec50330879d245fcaa
nanjekyejoannah <nanjekyejoannah@yahoo.com>
2014-11-17 11:24:03 +0300

    compiled assets locally

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -13,8 +13,7 @@
 //= require jquery
 //= require jquery_ujs
 //= require bootstrap.min
-//= require turbolinks
 //= require_tree 
-//= require dependent-rails
+
 
 
--- config/application.rb
+++ config/application.rb
@@ -8,24 +8,27 @@ require 'rails/all'
 Bundler.require(*Rails.groups)
 
 module WomenVRP
+
   class Application < Rails::Application
 
   	config.assets.initialize_on_precompile = false
     config.assets.paths << Rails.root.join("app", "assets", "fonts")
     config.assets.paths << Rails.root.join('vendor', 'assets', 'fonts')
 	config.assets.precompile << /\.(?:svg|eot|woff|ttf)$/
-  end
-end
 
-initializer 'setup_asset_pipeline', :group => :all do |app|
+	initializer 'setup_asset_pipeline', :group => :all  do |app|
   # We don't want the default of everything that isn't js or css, because it pulls too many things in
   app.config.assets.precompile.shift
+
   # Explicitly register the extensions we are interested in compiling
   app.config.assets.precompile.push(Proc.new do |path|
     File.extname(path).in? [
-      '.html', '.erb', '.haml', # Templates
-      '.png', '.gif', '.jpg', '.jpeg', # Images
-      '.eot', '.otf', '.svc', '.woff', '.ttf', # Fonts
+      '.html', '.erb', '.haml',                 # Templates
+      '.png',  '.gif', '.jpg', '.jpeg',         # Images
+      '.eot',  '.otf', '.svc', '.woff', '.ttf', # Fonts
     ]
   end)
 end
+  end
+end
+
