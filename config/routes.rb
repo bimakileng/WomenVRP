546ea4297663507f4b6236f0e02457a29e3c1611
nanjekyejoannah <nanjekyejoannah@yahoo.com>
2014-11-19 15:01:32 +0300

    added contacts route

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,10 @@
 Rails.application.routes.draw do
-	
+
 	resources :cases
 	get 'womanworker/home', to: 'womanworker#home'
 	get  'case/new', to: 'case#new'
 	post  'case/create', to: 'case#create'
 	get '/', to:'static#home'
-	get  '/about', to: 'static#about'  
+	get  '/about', to: 'static#about' 
+	get  '/contact', to: 'static#contact'  
 end
